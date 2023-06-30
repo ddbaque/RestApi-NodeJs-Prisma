4cdb47ccc7f6fa5c97194d1ea2b64c818aaaff79
tupapiloko <baquedavid0@gmail.com>
2023-06-30 14:57:32 +0200

    products delete finishe

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # RestApi-NodeJs-Prisma
 Simple rest api with ORM Prisma
+
+Repository Name => "RestApi-NodeJs-Prisma"
+
+//run server for development
+npm run dev
+
+//look Prisma Client in website
+npx prisma studio 
+
+//migrate databases
+npx prisma migrate dev
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,20 +4,13 @@ import prisma from "../db.js";
 
 const router = Router();
 
+//! --------------------- Get All Products -----------------------
 router.get("/products", async (req, res) => {
   const products = await prisma.product.findMany();
   res.json(products);
 });
 
-// ----- Create New Product
-router.post("/products", async (req, res) => {
-  const newProduct = await prisma.product.create({
-    data: req.body,
-  });
-  res.json(newProduct);
-});
-
-// ----- Get Product by Id
+//! --------------------- Get Product by Id ---------------------------------
 router.get("/products/:id", async (req, res) => {
   const id = +req.params.id;
   const productById = await prisma.product.findUnique({
@@ -25,7 +18,46 @@ router.get("/products/:id", async (req, res) => {
       id,
     },
   });
-  res.json(productById)
+  res.json(productById);
+});
+
+//! ----------------------- Create New Product ------------------------------
+router.post("/products", async (req, res) => {
+  const newProduct = await prisma.product.create({
+    data: req.body,
+  });
+  res.json(newProduct);
+});
+
+//! ----------------------- Update Product By Id ----------------------------
+router.put("/products/:id", async (req, res) => {
+  const id = +req.params.id;
+  const productUpdated = await prisma.product.update({
+    where: {
+      id,
+    },
+    data: req.body,
+  });
+  res.json(productUpdated);
+});
+
+//! ---------------------- Delete Product By Id -----------------------------
+router.delete("/products/:id", async (req, res) => {
+  const { id } = +req.params.id;
+
+  const product = await prisma.product.findFirst({
+    where: {
+      id,
+    },
+  });
+  const name = product.name
+  await prisma.product.delete({
+    where: {
+      name,
+    }
+  })
+
+  res.json(product);
 });
 
 export default router;
